1066ab1e44a4dee2d30f3811e72949c5d5a3701d
babji005 <84222740+babji005@users.noreply.github.com>
2021-05-17 09:45:14 +0530

    Delete New folder directory

--- New folder/postbook-spring/src/main/java/com/postbook/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.postbook;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-	User findByUsernameAndPassword(String username, String password);
-
-}
